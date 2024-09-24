d8d7ae3e2a08771762b3ad0165645d57cf752913
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-09-24 07:54:12 -0300

    feat: pegando conexão, apartir do config service

--- apps/queue/src/billing/payment-intent.processor.ts
+++ apps/queue/src/billing/payment-intent.processor.ts
@@ -5,15 +5,19 @@ import {
   OnModuleInit,
   OnModuleDestroy,
 } from '@nestjs/common';
-import { Worker, Job } from 'bullmq';
+import { Worker, Job, Queue } from 'bullmq';
 import { ConfigService } from '@nestjs/config';
+import { InjectQueue } from '@nestjs/bullmq';
 
 @Injectable()
 export class PaymentIntentProcessor implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(PaymentIntentProcessor.name);
   private worker: Worker;
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectQueue(BILLING_QUEUE) private readonly billingQueue: Queue,
+  ) {}
 
   async onModuleInit() {
     this.worker = new Worker(
@@ -30,11 +34,7 @@ export class PaymentIntentProcessor implements OnModuleInit, OnModuleDestroy {
         );
       },
       {
-        connection: {
-          host: this.configService.get('REDIS_HOST'),
-          port: this.configService.get('REDIS_PORT'),
-          password: this.configService.get('REDIS_PASSWORD'),
-        },
+        connection: this.billingQueue.opts.connection,
       },
     );
 
